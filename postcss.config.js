c2b947359751b602bf14b93ac146fe15160f80ad
heyitstonyg <145884746+heyitstonyg@users.noreply.github.com>
2024-12-04 20:35:12 -0800

    Configure PostCSS with proper plugins and VSCode settings

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('postcss').Config} */
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": "postcss-nesting",
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+  },
+};
